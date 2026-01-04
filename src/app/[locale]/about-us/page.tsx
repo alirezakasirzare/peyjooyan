62dbd4563ad88dd73a7406bc1bc2d46437bdbf4b
Alireza Kasirzare <alirezakasir@yahoo.com>
2026-01-04 12:25:38 +0330

    Add left panel component

--- src/app/[locale]/about-us/page.tsx
+++ src/app/[locale]/about-us/page.tsx
@@ -1,6 +1,11 @@
 import { Locale } from "next-intl";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 import Image from "next/image";
+import {
+  LeftPanel,
+  LeftPanelContent,
+  LeftPanelHeader,
+} from "~/components/common/left-panel";
 import { LandingNavs } from "~/components/layout/landing-navs";
 
 const AboutUsPage = async ({ params }: PageProps<"/[locale]/about-us">) => {
@@ -19,35 +24,38 @@ const AboutUsPage = async ({ params }: PageProps<"/[locale]/about-us">) => {
         className="w-full h-full object-cover fixed top-0 left-0"
       />
 
-      <div className="fixed left-0 top-0 bg-linear-to-l from-black to-black/0 w-full h-full bottom-22"></div>
+      <div className="fixed left-0 top-0 bg-linear-to-r from-black to-black/0 w-full h-full bottom-22"></div>
 
       <div className="relative z-10 container">
-        <div className="w-160 max-w-full pt-6 md:p-10">
-          <h2 className="font-title text-[60px] md:text-[120px] leading-none">
-            {t("title")}
-          </h2>
-          <h1 className="font-title text-[60px] md:text-[120px] text-primary leading-none">
-            {t("subtitle")}
-          </h1>
-          <h3 className="font-title text-xl text-primary leading-none mt-3">
-            {t("subtitle2")}
-          </h3>
-          <div className="mt-6 text-foreground/80 text-justify font-extralight">
+        <LeftPanel>
+          <LeftPanelHeader>
+            <h2 className="font-title text-[60px] md:text-[120px] leading-none">
+              {t("title")}
+            </h2>
+            <h1 className="font-title text-[60px] md:text-[120px] text-primary leading-none">
+              {t("subtitle")}
+            </h1>
+            <h3 className="font-title text-xl text-primary leading-none mt-3">
+              {t("subtitle2")}
+            </h3>
+          </LeftPanelHeader>
+          <LeftPanelContent>
+            <div className="mt-6 text-foreground/80 text-justify font-extralight"></div>
             <p>{t("text1")}</p>
             <p>{t("text2")}</p>
             <p>{t("text3")}</p>
             <p>{t("text4")}</p>
             <p>{t("text5")}</p>
-          </div>
 
-          <Image
-            src={"/images/miner2.png"}
-            alt="miner"
-            width={1200}
-            height={800}
-            className="mx-auto w-2/3"
-          />
-        </div>
+            <Image
+              src={"/images/miner2.png"}
+              alt="miner"
+              width={1200}
+              height={800}
+              className="mx-auto w-2/3"
+            />
+          </LeftPanelContent>
+        </LeftPanel>
       </div>
 
       <div className="fixed left-0 right-0 bottom-0 h-[120px] z-20 bg-linear-to-t from-black/75 via-black/60 to-transparent" />
---
+++ src/components/common/left-panel.tsx
@@ -0,0 +1,29 @@
+import { ScrollArea } from "../ui/scroll-area";
+
+export const LeftPanel = ({ children }: { children: React.ReactNode }) => (
+  <div className="w-160 max-w-full py-6 md:py-10 flex flex-col h-screen">
+    {children}
+  </div>
+);
+
+export const LeftPanelHeader = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return <div className="px-6 md:px-10 pb-4">{children}</div>;
+};
+
+export const LeftPanelContent = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => (
+  <ScrollArea
+    type="hover"
+    className="grow overflow-hidden"
+    scrollBarClassName="w-0"
+  >
+    <div className="px-6 md:px-10">{children}</div>
+  </ScrollArea>
+);
