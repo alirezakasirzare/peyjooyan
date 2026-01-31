b5c3a46961a883c1503fcfa165afc50b3ed07dcb
Alireza Kasirzare <alirezakasir@yahoo.com>
2026-01-31 12:26:23 +0330

    Fix footer page style

--- src/app/[locale]/footer/contact-list.tsx
+++ src/app/[locale]/footer/contact-list.tsx
@@ -1,4 +1,5 @@
 import { useLocale, useTranslations } from "next-intl";
+import Image from "next/image";
 import { CardSheet } from "~/components/ui/card-sheet";
 import data from "~/data/contact.json";
 
@@ -16,7 +17,7 @@ const ContactItem = ({
 }) => {
   return (
     <div>
-      <h1 className="font-title text-primary text-2xl">{title}</h1>
+      <h1 className="font-title text-primary md:text-2xl">{title}</h1>
       {children}
     </div>
   );
@@ -30,21 +31,29 @@ export const ContactList = () => {
   return (
     <div className="flex flex-col gap-8">
       <ContactItem title={t("addressTitle")}>
-        <p className="font-title">{address}</p>
+        <p className="font-title text-foreground/80 text-xs md:text-base">
+          {address}
+        </p>
         <CardSheet className="mt-4">
-          <div className="h-20"></div>
+          <Image
+            src={"/images/map.png"}
+            width={400}
+            height={300}
+            alt="map"
+            className="w-full object-cover"
+          />
         </CardSheet>
       </ContactItem>
 
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-3 gap-2 md:gap-4">
         <ContactItem title={t("emailTitle")}>
-          <p className="font-title">{data.email}</p>
+          <p className="font-title text-xs md:text-base">{data.email}</p>
         </ContactItem>
         <ContactItem title={t("contactTitle")}>
-          <p className="font-title">{data.phone}</p>
+          <p className="font-title text-xs md:text-base">{data.phone}</p>
         </ContactItem>
         <ContactItem title={t("linkedinTitle")}>
-          <p className="font-title">{data.linkedin}</p>
+          <p className="font-title text-xs md:text-base">{data.linkedin}</p>
         </ContactItem>
       </div>
     </div>
--- src/app/[locale]/footer/page.tsx
+++ src/app/[locale]/footer/page.tsx
@@ -17,15 +17,16 @@ const FooterPage = async ({ params }: PageProps<"/[locale]/about-us">) => {
 
   return (
     <>
-      <Image
-        src={"/images/about.png"}
-        alt="about"
-        width={1200}
-        height={800}
-        className="w-full h-full object-cover fixed top-0 left-0"
-      />
-
-      <div className="fixed left-0 top-0 bg-linear-to-r from-black to-black/0 w-full h-full bottom-22 -z-10"></div>
+      <div className="hidden md:block">
+        <Image
+          src={"/images/footer.png"}
+          alt="footer"
+          width={1200}
+          height={800}
+          className="w-100 h-full object-cover fixed bottom-0 right-0"
+        />
+        <div className="absolute inset-0 bg-linear-to-t from-black to-black/0 w-full h-full pointer-events-none" />
+      </div>
 
       <PanelContainer>
         <LeftPanel>
@@ -33,12 +34,29 @@ const FooterPage = async ({ params }: PageProps<"/[locale]/about-us">) => {
             <h2 className="font-title text-[60px] md:text-[110px] leading-none">
               {t("title")}
             </h2>
-            <h1 className="font-title text-[60px] md:text-[70px] leading-none">
+            <h1 className="font-title text-[20px] md:text-[70px] leading-none">
               {t("subtitle")}
             </h1>
           </LeftPanelHeader>
           <LeftPanelContent>
             <ContactList />
+            <div className="relative">
+              <Image
+                src={"/images/footer.png"}
+                alt="footer"
+                width={1200}
+                height={800}
+                className="w-100 h-full object-cover"
+              />
+              <Image
+                src={"/images/miner9.gif"}
+                alt="miner"
+                width={1200}
+                height={800}
+                className="w-40 object-cover absolute bottom-0 left-0"
+              />
+              <div className="absolute right-0 top-0 bg-linear-to-t from-black to-black/0 w-full h-full"></div>
+            </div>
           </LeftPanelContent>
         </LeftPanel>
       </PanelContainer>
