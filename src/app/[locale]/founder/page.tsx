57b1b923bf84e58623971d9cabcbd4754dd6228c
Alireza Kasirzare <alirezakasir@yahoo.com>
2026-02-04 20:41:10 +0330

    Fix founder page style

--- src/app/[locale]/founder/page.tsx
+++ src/app/[locale]/founder/page.tsx
@@ -11,6 +11,7 @@ import { PanelContainer } from "~/components/common/panel-container";
 import { Button } from "~/components/ui/button";
 import { FounderCta } from "./founder-cta";
 import { FounderPopupSection } from "./founder-popup-section";
+import { ArrowIcon } from "~/components/sections/arrow-icon";
 
 const FounderPage = async ({ params }: PageProps<"/[locale]/founder">) => {
   const { locale } = await params;
@@ -26,12 +27,12 @@ const FounderPage = async ({ params }: PageProps<"/[locale]/founder">) => {
           alt="founder"
           width={1200}
           height={800}
-          className="absolute top-0 md:top-auto md:bottom-0 right-0 rtl:right-auto rtl:left-0 w-80 md:w-160 rtl:-scale-x-100"
+          className="absolute top-0 md:top-auto md:bottom-0 -end-13 md:end-0 w-80 md:w-160 rtl:-scale-x-100"
         />
         <div className="md:hidden absolute inset-0 bg-linear-to-t from-black to-black/0 w-full h-1/2 pointer-events-none" />
       </div>
 
-      <div className="fixed right-0 top-0 bg-linear-to-r from-black to-black/0 w-160 h-full"></div>
+      <div className="fixed end-0 top-0 bg-linear-to-r rtl:bg-linear-to-l from-black to-black/0 w-160 h-full"></div>
 
       <PanelContainer>
         <LeftPanel className="gap-32 md:gap-0">
@@ -39,21 +40,21 @@ const FounderPage = async ({ params }: PageProps<"/[locale]/founder">) => {
             <h2 className="font-title text-[60px] md:text-[90px] leading-none">
               {t("title")}
             </h2>
-            <h1 className="font-title text-lg md:text-[60px] rtl:md:text-[30px] text-primary leading-none">
+            <h1 className="font-title text-lg md:text-[60px] rtl:md:text-[30px] text-primary leading-none -mt-1 md:-mt-3 rtl:mt-2 rtl:md:mt-1">
               {t("subtitle")}
             </h1>
-            <h3 className="font-title text-lg md:text-xl leading-none mt-3">
+            <h3 className="font-title text-lg md:text-xl leading-none mt-1 rtl:mt-1 rtl:md:mt-1">
               2012 | 2020
             </h3>
-            <div className="flex gap-4 mt-5">
+            <div className="flex gap-4 mt-2">
               <FounderCta />
               <Button size={"icon"} variant={"outline"}>
-                <ArrowUpRightIcon className="size-4 fill-primary" />
+                <ArrowIcon />
               </Button>
             </div>
           </LeftPanelHeader>
           <LeftPanelContent>
-            <div className="text-foreground/80 text-justify font-extralight">
+            <div className="text-foreground/60 text-justify font-extralight flex flex-col gap-3">
               <p>{t("text1")}</p>
               <p>{t("text2")}</p>
               <p>{t("text3")}</p>
