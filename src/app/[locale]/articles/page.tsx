234c853b60bbae3afb94b32b66dd054f93824280
Alireza Kasirzare <alirezakasir@yahoo.com>
2026-02-04 21:12:35 +0330

    Fix articles page style

--- src/app/[locale]/articles/page.tsx
+++ src/app/[locale]/articles/page.tsx
@@ -10,6 +10,7 @@ import { PanelContainer } from "~/components/common/panel-container";
 import { Button } from "~/components/ui/button";
 import { ArticleList } from "./article-list";
 import { DesktopContent } from "./desktop-content";
+import Image from "next/image";
 
 const ArticlesPage = async ({ params }: PageProps<"/[locale]/about-us">) => {
   const { locale } = await params;
@@ -19,16 +20,24 @@ const ArticlesPage = async ({ params }: PageProps<"/[locale]/about-us">) => {
 
   return (
     <>
+      <Image
+        src={"/images/star.png"}
+        alt="star"
+        width={1200}
+        height={800}
+        className="w-full h-2/3 md:h-screen fixed top-0 left-0 object-contain"
+      />
+
       <PanelContainer>
         <LeftPanel>
           <LeftPanelHeader>
             <h2 className="font-title text-[60px] md:text-[120px] leading-none">
               {t("title")}
             </h2>
-            <h1 className="font-title text-[20px] md:text-[55px] text-primary leading-none">
+            <h1 className="font-title text-[20px] md:text-[55px] text-primary leading-none -mt-1 md:-mt-3 rtl:-mt-2 rtl:md:-mt-6">
               {t("subtitle")}
             </h1>
-            <div className="flex gap-4 mt-5">
+            <div className="flex gap-4 mt-3">
               <Button
                 className="font-title rtl:font-text px-5"
                 variant={"outline-secondary"}
