cadd2e24829035b5293eeb7faf757f355c016fbe
Alireza Kasirzare <alirezakasir@yahoo.com>
2026-02-25 10:06:23 +0330

    Fix default locale

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,10 +2,12 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en", "fa"],
+  locales: ["fa", "en"],
 
   localePrefix: "as-needed",
 
   // Used when no locale matches
   defaultLocale: "fa",
+
+  localeDetection: false,
 });
